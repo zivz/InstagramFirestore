72a2ec2d618922ac48f67d270e4b107b68b0f55b
Ziv Zalzstein <zivzaltz@gmail.com>
2021-01-23 15:18:09 +0200

    Initial Commit

---
+++ InstagramFireStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InstagramFireStore
+//
+//  Created by Zalzstein, Ziv on 23/01/2021.
+//  Copyright © 2021 Zalzstein, Ziv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
